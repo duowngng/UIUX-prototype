0ec82af67eaef0333194084a5d049060355a5284
gioidv-vnist <gioidv.vnist@gmail.com>
2024-05-15 16:26:49 +0700

    change: type card on dashboard

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,14 @@
 /** @format */
 
 import React from "react";
-import { LucideIcon } from "lucide-react";
+import { LucideIcon,Crosshair,Clock3,Calendar} from "lucide-react";
 import { cn } from "@/lib/utils";
-
+import { Progress } from "@/components/ui/progress";
 export type CardProps = {
   label: string;
   icon: LucideIcon;
   amount: string;
-  discription: string;
+  progress: number;
 };
 
 export default function Card(props: CardProps) {
@@ -17,17 +17,26 @@ export default function Card(props: CardProps) {
       <section className="flex justify-between gap-2">
         {/* label */}
         <p className="text-sm">{props.label}</p>
-        {/* icon */}
-        <props.icon className="h-4 w-4 text-gray-400" />
-      </section>
+      </section>        
       <section className="flex flex-col gap-1">
         <h2 className="text-2xl font-semibold">{props.amount}</h2>
-        <p className="text-xs text-gray-500">{props.discription}</p>
+      </section>
+      <section className="flex justify-between gap-2 items-center">
+        <Progress value={props.progress} />
+        <p>{props.progress}%</p>
+        </section>
+      <section className="flex flex-col gap-1">
+        <div className="flex justify-between gap-2 items-center">
+        Crosshair;,Clock3,Calendar
+        
+          <Progress value={props.progress} />
+          <p>{props.progress}%</p>
+        </div>
       </section>
     </CardContent>
   );
 }
-
+ 
 export function CardContent(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
